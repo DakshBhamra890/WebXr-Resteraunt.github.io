2c88a47bc09c704b571aaecd8efe963e2e7b6f5d
DakshBhamra890 <bhamradaksh@gmail.com>
2025-03-30 19:51:32 -0400

    commit 3
    adding webXr experience code and sound

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas, true);
 
 // create the scene
-const createScene = function() {
+const createScene = async function() {
     const scene = new BABYLON.Scene(engine);
     // creating a camera
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 15, new BABYLON.Vector3(0, 0, 0));
@@ -14,7 +14,7 @@ const createScene = function() {
     light.intensity = 0.7;
     
     // creating a ground
-    const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, scene);
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 15, height: 15 }, scene);
    // creating the chess like ground
     const groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
     const groundDesign = new BABYLON.Texture("images/10071154.jpg", scene);
@@ -22,6 +22,8 @@ const createScene = function() {
     groundMaterial.diffuseTexture = groundDesign;
     ground.material = groundMaterial;
 
+    let wallmaterial = new BABYLON.StandardMaterial("wallmaterial", scene);
+    wallmaterial.diffuseColor = new BABYLON.Color3(0.9, 0.6, 0.5); // Red color
     // adding walls to the scene
     const wHeight = 6.5;
     const wThickness = 0.5;
@@ -31,14 +33,19 @@ const createScene = function() {
 
     const wBack1 = BABYLON.MeshBuilder.CreateBox("wBack1", { width: wbackLength, height: wHeight, depth: wThickness }, scene);
     wBack1.position.z = -5, wBack1.position.y = 3.25, wBack1.position.x = 3;
+    wBack1.material = wallmaterial;
     const wBack2 = BABYLON.MeshBuilder.CreateBox("wBack2", { width: wbackLength, height: wHeight, depth: wThickness }, scene);
     wBack2.position.z = -5, wBack2.position.y = 3.25, wBack2.position.x = -3;
+    wBack2.material = wallmaterial;
     const wfront = BABYLON.MeshBuilder.CreateBox("wfront", { width: wLength, height: wHeight, depth: wThickness }, scene);
     wfront.position.z = 5, wfront.position.y = 3.25;
+    wfront.material = wallmaterial;
     const wLeft = BABYLON.MeshBuilder.CreateBox("wLeft", {  depth: wLength,  height: wHeight , width: wThickness}, scene);
     wLeft.position.x = -5, wLeft.position.y = 3.25;
+    wLeft.material = wallmaterial;
     const wRight = BABYLON.MeshBuilder.CreateBox("wRight", {  depth: wLength , height: wHeight, width: wThickness }, scene);
     wRight.position.x = 5, wRight.position.y = 3.25;
+    wRight.material = wallmaterial;
 
 // adding the roof
     const roofHeight = 10.5;
@@ -47,6 +54,7 @@ const createScene = function() {
     const roofFlat = BABYLON.MeshBuilder.CreateBox("wRight", {  depth: roofLength , height: roofHeight, width: roofThickness }, scene);
     roofFlat.rotation.z = Math.PI / 2;
     roofFlat.position = new BABYLON.Vector3(0, 6.5, 0);
+    roofFlat.material = wallmaterial;
 
     const rooftriangleH = 9.5;
     const rooftriangltT = 0.5;
@@ -55,10 +63,12 @@ const createScene = function() {
     const roofTraingle1 = BABYLON.MeshBuilder.CreateBox("wRight", {  depth: rooftraingleL , height: rooftriangleH, width: rooftriangltT }, scene);
     roofTraingle1.rotation.z = Math.PI / 3;
     roofTraingle1.position = new BABYLON.Vector3(4, 7.5, 0);
+    roofTraingle1.material = wallmaterial;
     
     const roofTraingle2 = BABYLON.MeshBuilder.CreateBox("wRight", {  depth: rooftraingleL , height: rooftriangleH, width: rooftriangltT }, scene);
     roofTraingle2.rotation.z = Math.PI / -3;
     roofTraingle2.position = new BABYLON.Vector3(-4, 7.5, 0);
+    roofTraingle2.material = wallmaterial;
 
 
     // adding the chandelier model 
@@ -217,26 +227,23 @@ const createScene = function() {
             console.error("Error loading mesh:", error);
             }
         ),
-        // adding the doir
-        BABYLON.SceneLoader.ImportMesh(
-            null,      
-            "./",      
-            "DOOR.glb", 
-            scene,
-            function (meshes) {
-                console.log("Meshes loaded:", meshes);
-                meshes.forEach((mesh) => {
-                    mesh.scaling = new BABYLON.Vector3(1, 1, 1);
-                    mesh.position = new BABYLON.Vector3(1 , 1, 1);
-                    mesh.rotation.y = Math.PI;
-                });
-            },
-        function (error) { 
-            console.error("Error loading mesh:", error);
-            }
-        )
+        // adding a sound in the scene
 
 );
+const sound = new BABYLON.Sound("restraunt", "./29560__dude3966__restraunt2.wav", scene, null, {
+    loop: true,
+    autoplay: true
+});
+
+// adding the webXR experience
+if ( await BABYLON.WebXRSessionManager.IsSessionSupportedAsync("immersive-vr")) {
+    const xr = await scene.createDefaultXRExperienceAsync({
+    floorMeshes: [ground],
+    optionalFeatures: true
+    });
+    } else {
+    console.log("WebXR is not supported on this device.");
+};
     return scene;
 };
 
